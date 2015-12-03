67527098755f587a69bda8474dac81efff9a8cd3
Ted Senft <ted.senft@gmail.com>
2015-12-03 00:59:14 -0500

    [feature] - Started implementation of information_request event. Note that the information_response event is not yet implemented- this event only handles the generation of the response, not the routing. Location information is now ready to be used- pass it a valid identifier to get the name, type, parent, and perhaps a few other values later on.

--- Development/Application Logic/Events/Handlers/InformationRequest.cs
+++ Development/Application Logic/Events/Handlers/InformationRequest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json.Linq;
+using Ostenvighx.Suibhne.Networks.Base;
 
 namespace Ostenvighx.Suibhne.Events.Handlers {
 
@@ -13,9 +14,60 @@ namespace Ostenvighx.Suibhne.Events.Handlers {
     internal class InformationRequest : EventHandler {
 
         public void HandleEvent(JObject json) {
-            
+            JObject returnData = new JObject();
+            if (json["extid"] == null && json["netid"] == null)
+                throw new FormatException("Need to have either extid or netid defined for an information request.");
+
+            if (json["token"] == null || json["token"].ToString() == "")
+                throw new FormatException("Need a token defined to route the response back effectively.");
+
+            returnData.Add("event", "information_response");
+
+            if (json["extid"] != null)
+                returnData.Add("extid", json["extid"]);
+
+            if (json["netid"] != null)
+                returnData.Add("netid", json["netid"]);
+
+            returnData.Add("token", json["token"]);
+
+            if (json["type"] == null || json["type"].ToString().Trim() == "")
+                throw new FormatException("An information type request is needed. Valid types are 'extension', 'location', or 'system'.");
+
+            // The type determines which type of information is being requested.
+            switch (json["type"].ToString().ToLower()) {
+                case "extension":
+
+                    break;
+
+                case "location":
+                    if (json["location"] == null)
+                        throw new FormatException("Need to specify a location guid.");
+
+                    Guid loc;
+                    try { loc = Guid.Parse(json["location"].ToString()); }
+                    catch (Exception) { throw new FormatException("Location is not a valid guid.");  }
+
+                    Location location = LocationManager.GetLocationInfo(loc);
+
+                    JObject locationj = new JObject();
+                    locationj.Add("name", location.Name);
+                    locationj.Add("type", (byte) location.Type);
+                    if (location.Parent != Guid.Empty)
+                        locationj.Add("parent", location.Parent);
+
+                    returnData.Add("location", locationj);
+                    break;
+
+                case "system":
+                    if (json["node"] == null)
+                        throw new FormatException("Need a NODE defined to get information for.");
 
 
+                    break;
+            }
+
+            EventManager.HandleInternalEvent(returnData);
         }
     }
 }
